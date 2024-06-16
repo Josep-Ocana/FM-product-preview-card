8a06100793b6abbbddf4036b5290feaf69ffe0ae
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2024-06-16 18:47:39 +0200

    fisrt commit. Installed Node.js, Sass, Webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: '.src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
+	devServer: {
+		contentbase: path.resolve(__dirname, 'dist'),
+		compress: true,
+		port: 9000,
+	},
+};
